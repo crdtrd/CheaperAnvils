e6f9fb78db6af3ab48d6156c3397ebcdafb53a1f
crdtrd <crdtrd@gmail.com>
2025-08-10 00:40:20 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/cheaperanvils/CheaperAnvils.java
@@ -0,0 +1,10 @@
+package com.drtdrc.cheaperanvils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CheaperAnvils implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
